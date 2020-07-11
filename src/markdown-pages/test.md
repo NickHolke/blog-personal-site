5bd25d47e2b46d383b201bd3f3461bcf77a1d7aa
Nick Holke <nickholke@gmail.com>
2020-07-11 16:25:49 -0700

    Added images to markdown

--- src/markdown-pages/test.md
+++ src/markdown-pages/test.md
@@ -2,6 +2,8 @@
 slug: "/blog/my-first-post"
 date: "2019-05-04"
 title: "My first blog post"
+featuredImage: "../images/gatsby-icon.png"
 ---
 
-Here is some text for this example blog post!
+Here is some text for this example blog post! And now there should be an image underneath.
+![Example Image](https://images.unsplash.com/photo-1482849297070-f4fae2173efe?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80)
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
+export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
+  const featuredImageFluid = frontmatter.featuredImage.childImageSharp.fluid
+
+  console.log(featuredImageFluid)
   return (
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
+        <Img fluid={featuredImageFluid} />
         <h2>{frontmatter.date}</h2>
         <div
           className="blog-post-content"
@@ -28,6 +31,13 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         slug
         title
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
